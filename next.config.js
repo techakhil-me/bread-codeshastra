311a85421fd396913c654f41b682bf757b98cff4
ST1LLWATER <62516824+ST1LLWATER@users.noreply.github.com>
2022-03-27 13:08:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
       {
         // matching all API routes
         source: '/api/:path*',
-        destination: `https://bread-backend.herokuapp.com/:path*`,
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
