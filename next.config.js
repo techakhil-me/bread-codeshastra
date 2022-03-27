8e9d0cd6143dc829e87afa49f55f29e1e5da58c2
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2022-03-27 11:37:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     return [
       {
         // matching all API routes
+        source: '/:path*',
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
